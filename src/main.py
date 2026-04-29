fa3c4c90ae2a0590f5b48ae27569fe9690dc07bf
Neel-Asher <neel.asher20@gmail.com>
2026-04-29 12:01:15 +0530

    [FEAT]: Performed Data Transformation for Stage 3

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
+# STAGE 1: Data Loading and Initial Exploration
 deliveries_df = pd.read_csv("../data/deliveries.csv")
 matches_df = pd.read_csv("../data/matches.csv")
 
 print("Datasets are loaded successfully.")
 
+# STAGE 2: Data Validation and Cleaning
 print("Checking for missing values: ")
 
 print("Deliveries missing values:")
@@ -43,3 +45,38 @@ print("Final dataframe shapes: ")
 
 print(f"\nDeliveries Shape: {deliveries_df.shape}")
 print(f"Matches Shape: {matches_df.shape}")
+
+# STAGE 3: Data Transformation
+print("Starting data transformation: ")
+
+deliveries_df.columns = deliveries_df.columns.str.lower().str.strip()
+matches_df.columns = matches_df.columns.str.lower().str.strip()
+
+print("\nColumn names standardized.")
+
+deliveries_df["total_runs"] = (
+    deliveries_df["batsman_runs"] +
+    deliveries_df["extra_runs"]
+)
+
+print("\nNew column 'total_runs' created.")
+
+merged_df = deliveries_df.merge(
+    matches_df,
+    left_on="match_id",
+    right_on="id",
+    how="inner"
+)
+
+print("\nDatasets merged successfully.")
+
+print("Merged dataset info: ")
+
+print("\nMerged Shape:")
+print(merged_df.shape)
+
+print("\nMerged Columns:")
+print(merged_df.columns)
+
+print("\nFirst 5 Rows:")
+print(merged_df.head())
