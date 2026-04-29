7dcb38b2e67914df3bfc783d8293eb4592b192c4
Neel-Asher <neel.asher20@gmail.com>
2026-04-29 11:51:12 +0530

    [FEAT]: Executed Data Cleaning & Validation for Stage 2

--- src/main.py
+++ src/main.py
@@ -3,14 +3,43 @@ import pandas as pd
 deliveries_df = pd.read_csv("../data/deliveries.csv")
 matches_df = pd.read_csv("../data/matches.csv")
 
-print("DELIVERIES DATASET")
-print(deliveries_df.shape)
-print(deliveries_df.columns)
+print("Datasets are loaded successfully.")
+
+print("Checking for missing values: ")
+
+print("Deliveries missing values:")
+print(deliveries_df.isnull().sum())
+
+print("Matches missing values:")
+print(matches_df.isnull().sum())
+
+print("Validating match id alignment: ")
+
+deliveries_match_ids = set(deliveries_df["match_id"].unique())
+matches_ids = set(matches_df["id"].unique())
+
+missing_in_matches = deliveries_match_ids - matches_ids
+missing_in_deliveries = matches_ids - deliveries_match_ids
+
+print(f"\nMatch IDs in deliveries but not in matches: {len(missing_in_matches)}")
+print(f"Match IDs in matches but not in deliveries: {len(missing_in_deliveries)}")
+
+print("Validating data tyoes: ")
+
+print("\nDeliveries Data Types:\n")
 print(deliveries_df.dtypes)
 
-print("")
-
-print("MATCHES DATASET")
-print(matches_df.shape)
-print(matches_df.columns)
+print("\nMatches Data Types:\n")
 print(matches_df.dtypes)
+
+print("Performing basic cleaning: ")
+
+matches_df["winner"] = matches_df["winner"].fillna("No Result")
+matches_df["player_of_match"] = matches_df["player_of_match"].fillna("Not Awarded")
+
+print("\nBasic cleaning completed.")
+
+print("Final dataframe shapes: ")
+
+print(f"\nDeliveries Shape: {deliveries_df.shape}")
+print(f"Matches Shape: {matches_df.shape}")
